7bf6637b22141f02413445039ef61898c6792094
Gabriel <ggabrielcunha06@gmail.com>
2025-03-12 18:21:46 -0300

    meus times favoritos

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("ＣＯ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("ＲＩ");
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("ＮＴ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("ＨＩ");
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("ＡＮ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Ｓ");
+
+Console.ResetColor();
+
+Console.BackgroundColor = ConsoleColor.Black;
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.Write("ＭＩ");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.Write("ＬＷ");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.Write("ＡＵ");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.Write("ＫＥ");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.Write("Ｅ ");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.Write("ＢＵ");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.Write("ＣＫ");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.Write("Ｓ");
+
+Console.ResetColor();
